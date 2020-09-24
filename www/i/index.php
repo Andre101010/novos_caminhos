584031f04ff1b855999ce11c2a0f10906d8be507
Andre101010 <andreplay-10@hotmail.com>
2020-09-24 14:38:58 -0300

    first commit

---
+++ www/i/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang='pt-br'>
+
+<head>
+    <meta charset='UTF-8'>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+
+    <title>FSPHP: Iniciando um Projeto"</title>
+    <link rel="stylesheet" href="assets/style.css" />
+
+</head>
+
+<body>
+    <?php
+
+    //$start * "Vamos Começar!*";
+    //echo "<h1>Olá Mundo! {$start}</h1>";
+    echo "<p id='js'>Loading ...</p>";
+    ?>
+    <script src="assets/scripts.js"></script>
+</body>
+
+</html>
